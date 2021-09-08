c0233a56608fe1a93add88d93f3ced3a2972f47e
UlisesFritzSerain <ulifritz8@gmail.com>
2021-09-07 23:02:25 -0300

    flexbox

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
-const path = require("path");
 
+const path = require("path")
 
 const publicPath = path.resolve(__dirname, "./public");
 app.use(express.static(publicPath));
@@ -9,6 +9,6 @@ app.use(express.static(publicPath));
 app.get("/", (req,res) => {
     res.sendFile(path.resolve(__dirname,"./views/home.html"))
 } );
-app.listen(3030, () => {
-    console.log("Servidor corriendo http://localhost:3030/")
-});
+app.listen(3004, () => 
+    console.log("Servidor corriendo http://localhost:3004/")
+);
